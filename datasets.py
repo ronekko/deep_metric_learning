f7c856ec0628fbab97c22c7a51c0d6cb5ecf7231
sakurai <sakurai@ais.ics.ritsumei.ac.jp>
2017-01-10 01:38:52 +0900

    Extract common part

--- datasets.py
+++ datasets.py
@@ -13,19 +13,24 @@ from random_fixed_size_crop_mod import RandomFixedSizeCrop
 from cars196_dataset import Cars196Dataset
 
 
-def get_cars196_streams(crop_size=227, load_in_memory=False):
+def make_random_cropped_stream(dataset, crop_size):
     # This scheme is dummy, since DataStream requires an iteration_scheme for
     # DataStream.produces_examples to be False in the constructor.
     dummy_scheme = SequentialScheme(1, 1)
 
-    train_stream = RandomFixedSizeCrop(
-        DataStream(Cars196Dataset(['train'], load_in_memory=load_in_memory),
-                   iteration_scheme=dummy_scheme),
+    stream = RandomFixedSizeCrop(
+        DataStream(dataset, iteration_scheme=dummy_scheme),
         (crop_size, crop_size), which_sources=("images"))
-    test_stream = RandomFixedSizeCrop(
-        DataStream(Cars196Dataset(['test'], load_in_memory=load_in_memory),
-                   iteration_scheme=dummy_scheme),
-        (crop_size, crop_size), which_sources=("images"))
+    return stream
+
+
+def get_cars196_streams(crop_size=227, load_in_memory=False):
+    train_dataset = Cars196Dataset(['train'], load_in_memory=load_in_memory)
+    train_stream = make_random_cropped_stream(train_dataset, crop_size)
+
+    test_dataset = Cars196Dataset(['test'], load_in_memory=load_in_memory)
+    test_stream = make_random_cropped_stream(test_dataset, crop_size)
+
     return train_stream, test_stream
 
 
