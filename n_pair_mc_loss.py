d3933d58b2ebcb0fb0c6301344335ae018973774
sakurai <sakurai@ais.ics.ritsumei.ac.jp>
2017-01-29 23:01:47 +0900

    Modify to average the L2 norm loss of output vectors

--- n_pair_mc_loss.py
+++ n_pair_mc_loss.py
@@ -27,6 +27,7 @@ def n_pair_mc_loss(f, f_p, l2_reg):
     N = len(logit.data)
     xp = cuda.get_array_module(logit.data)
     loss_sce = softmax_cross_entropy(logit, xp.arange(N))
-    l2_loss = sum(batch_l2_norm_squared(f) + batch_l2_norm_squared(f_p))
+    l2_loss = sum(batch_l2_norm_squared(f) +
+                  batch_l2_norm_squared(f_p)) / (2.0 * N)
     loss = loss_sce + l2_reg * l2_loss
     return loss
