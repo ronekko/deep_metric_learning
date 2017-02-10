a969f342137485dbb6f212bc1aa320770aac1421
sakurai <sakurai@ais.ics.ritsumei.ac.jp>
2017-02-11 01:01:36 +0900

    Add fuel dataset for CUB200_2011

---
+++ cub200_2011_dataset.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 11 00:57:05 2017
+
+@author: sakurai
+"""
+
+from fuel.datasets import H5PYDataset
+from fuel.utils import find_in_data_path
+from fuel.schemes import SequentialScheme
+from fuel.streams import DataStream
+
+
+class Cub200_2011Dataset(H5PYDataset):
+
+    _filename = 'cub200_2011/cub200_2011.hdf5'
+
+    def __init__(self, which_sets, **kwargs):
+        super(Cub200_2011Dataset, self).__init__(
+            file_or_path=find_in_data_path(self._filename),
+            which_sets=which_sets, **kwargs)
+
+
+def load_as_ndarray(which_sets=['train', 'test']):
+    datasets = []
+    for split in which_sets:
+        data = Cub200_2011Dataset([split], load_in_memory=True).data_sources
+        datasets.append(data)
+    return datasets
+
+
+if __name__ == '__main__':
+    dataset = Cub200_2011Dataset(['train'])
+
+    st = DataStream(
+        dataset, iteration_scheme=SequentialScheme(dataset.num_examples, 1))
+    it = st.get_epoch_iterator()
+    it.next()
