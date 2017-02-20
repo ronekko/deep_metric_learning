2d27ac9c27987d3801734706043f98a5a9fa1864
sakurai <sakurai@ais.ics.ritsumei.ac.jp>
2017-02-20 21:46:09 +0900

    Revert "Merge branch 'off-memory-datasets'"
    This reverts commit 276afdf9e9cf26b3a76b5142b3d2a0baf2412901, reversing
    changes made to 5c17250a77b7a58e0b4512a15866f3ffbcd6cbb0.

--- datasets/dataset_loader.py
+++ datasets/dataset_loader.py
@@ -9,7 +9,6 @@ import random
 
 import numpy as np
 from chainer.dataset import DatasetMixin
-from fuel.datasets import H5PYDataset
 
 import cars196_dataset
 import cub200_2011_dataset
@@ -28,33 +27,22 @@ class RandomCropFlipDataset(DatasetMixin):
        crop_size (tuple):
            height and width as tuple of integers
     '''
-    def __init__(self, dataset, crop_size=(224, 224)):
-        if isinstance(dataset, H5PYDataset):
-            length = dataset.num_examples
-            is_fuel = True
-        elif isinstance(dataset, tuple):
-            length = len(dataset[0])
-            if len(dataset[1]) != length:
-                raise ValueError("images and labels must have the same length")
-            is_fuel = False
-        else:
-            ValueError("datasets must be H5PYDataset or a tuple of two "
-                       "ndarrays, i.e. (images, labels)")
-        self._dataset = dataset
+    def __init__(self, images, labels, crop_size=(224, 224)):
+        length = len(images)
+        if len(labels) != length:
+            raise ValueError("images and labels must have the same length")
+        self._images = images
+        self._labels = labels
         self._length = length
         self._crop_size = crop_size
-        self._is_fuel = is_fuel
 
     def __len__(self):
         return self._length
 
     def get_example(self, i):
-        if self._is_fuel:
-            image, label = self._dataset[i]
-        else:
-            image, label = [array[i] for array in self._dataset]
+        image = self._images[i]  # a chw-shaped image
+        label = self._labels[i]
 
-        # image of chw-shape
         _, h, w = image.shape
         crop_size_h, crop_size_w = self._crop_size
         # Randomly crop a region and flip the image
@@ -70,60 +58,38 @@ class RandomCropFlipDataset(DatasetMixin):
 
         return image, label
 
-    def get_labels(self):
-        dataset = self._dataset
-        if self._is_fuel:
-            for i, a_sample in enumerate(dataset[0]):
-                if a_sample.ndim == 0 or 1 in a_sample.shape:
-                    label_axis = i
-                    break
-            if dataset.load_in_memory:
-                return dataset.data_sources[label_axis]
-            else:
-                num_examples = len(self)
-                batch_size = 100
-                labels = []
-                for i in range(0, num_examples, batch_size):
-                    i_end = min(i + batch_size, num_examples)
-                    labels.append(dataset[i:i_end][label_axis])
-                return np.concatenate(labels)
-        else:
-            return dataset[1]
 
-
-def make_n_pairs_mc_iterator(dataset, batch_size, multiprocess=False,
+def make_n_pairs_mc_iterator(x, labels, batch_size, multiprocess=False,
                              repeat=True):
-    chainer_dataset = RandomCropFlipDataset(dataset)
-    num_batches = len(chainer_dataset) / batch_size
-    labels = chainer_dataset.get_labels()
+    dataset = RandomCropFlipDataset(x, labels)
+    num_batches = len(dataset) / batch_size
     order_sampler = NPairMCIndexesSampler(labels, batch_size, num_batches)
     if multiprocess:
-        it = MultiprocessIterator(chainer_dataset, batch_size, repeat=repeat,
+        it = MultiprocessIterator(dataset, batch_size, repeat=repeat,
                                   n_processes=3, order_sampler=order_sampler)
     else:
-        it = SerialIterator(chainer_dataset, batch_size, repeat=repeat,
+        it = SerialIterator(dataset, batch_size, repeat=repeat,
                             order_sampler=order_sampler)
     return it
 
 
-def make_simple_iterator(dataset, batch_size, repeat=False, shuffle=False,
+def make_simple_iterator(x, labels, batch_size, repeat=False, shuffle=False,
                          multiprocess=False):
-    chainer_dataset = RandomCropFlipDataset(dataset)
+    dataset = RandomCropFlipDataset(x, labels)
     if multiprocess:
-        it = MultiprocessIterator(chainer_dataset, batch_size, repeat=repeat,
+        it = MultiprocessIterator(dataset, batch_size, repeat=repeat,
                                   shuffle=shuffle, n_processes=3)
     else:
-        it = SerialIterator(chainer_dataset, batch_size, repeat=repeat,
+        it = SerialIterator(dataset, batch_size, repeat=repeat,
                             shuffle=shuffle)
     return it
 
 
-def make_epoch_iterator(dataset, batch_size, multiprocess=False):
-    return make_simple_iterator(dataset, batch_size, multiprocess)
+def make_epoch_iterator(x, labels, batch_size, multiprocess=False):
+    return make_simple_iterator(x, labels, batch_size, multiprocess)
 
 
-def get_iterators(batch_size=50, dataset='cars196', method='n_pairs_mc',
-                  load_in_memory=False):
+def get_iterators(batch_size=50, dataset='cars196', method='n_pairs_mc'):
     '''
     args:
        batch_size (int):
@@ -135,36 +101,48 @@ def get_iterators(batch_size=50, dataset='cars196', method='n_pairs_mc',
     '''
 
     if dataset == 'cars196':
-        dataset_class = cars196_dataset.Cars196Dataset
+        dataset_module = cars196_dataset
     elif dataset == 'cub200_2011':
-        dataset_class = cub200_2011_dataset.Cub200_2011Dataset
+        dataset_module = cub200_2011_dataset
     elif dataset == 'products':
-        dataset_class = online_products_dataset.OnlineProductsDataset
+        dataset_module = online_products_dataset
     else:
         raise ValueError(
             "`dataset` must be 'cars196', 'cub200_2011' or 'products'.")
 
-    train = dataset_class(['train'], load_in_memory=load_in_memory)
-    test = dataset_class(['test'], load_in_memory=load_in_memory)
-
+    train, test = dataset_module.load_as_ndarray(['train', 'test'])
     if method == 'n_pairs_mc':
-        iter_train = make_n_pairs_mc_iterator(train, batch_size)
+        iter_train = make_n_pairs_mc_iterator(train[0], train[1], batch_size)
     elif method == 'clustering':
-        iter_train = make_simple_iterator(train, batch_size,
+        iter_train = make_simple_iterator(train[0], train[1], batch_size,
                                           repeat=True, shuffle=True)
     else:
         raise ValueError("`method` must be 'n_pairs_mc' or 'clustering'.")
-    iter_train_eval = make_epoch_iterator(train, batch_size)
-    iter_test = make_epoch_iterator(test, batch_size)
+    iter_train_eval = make_epoch_iterator(train[0], train[1], batch_size)
+    iter_test = make_epoch_iterator(test[0], test[1], batch_size)
     return iter_train, iter_train_eval, iter_test
 
 
 if __name__ == '__main__':
+    multiprocess = False
     batch_size = 50
-    dataset = 'cub200_2011'  # 'cars196' or 'cub200_2011' or 'products'
-    method = 'n_pairs_mc'  # 'n_pairs_mc' or 'clustering'
-    load_in_memory = False
+    train = cars196_dataset.load_as_ndarray(['train'])[0]
+    x, labels = train
+    dataset = RandomCropFlipDataset(x, labels)
+    num_batches = len(dataset) / batch_size
+    order_sampler = NPairMCIndexesSampler(labels, batch_size, num_batches)
+    if multiprocess:
+        it = MultiprocessIterator(dataset, batch_size, repeat=True,
+                                  n_processes=3,
+                                  order_sampler=order_sampler)
+    else:
+        it = SerialIterator(dataset, batch_size, repeat=True,
+                            order_sampler=order_sampler)
 
-    it = get_iterators(batch_size, dataset, method, load_in_memory)
-    it_train, it_train_eval, it_test = it
-    batch = next(it_train)
+    for i in range(1):
+        batch = next(it)
+        l = np.ravel([pair[1] for pair in batch]).tolist()
+        print i
+        print l[:batch_size/2]
+        print l[batch_size/2:]
+        print
